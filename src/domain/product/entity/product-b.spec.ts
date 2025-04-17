986aca449b69d8e8e6a2bbcffb8d5a2436e804ec
Lucca Gabriel Dias Barbosa <lucca@MacBook.local>
2025-04-17 20:27:27 -0300

    feat: product validator

--- src/domain/product/entity/product-b.spec.ts
+++ src/domain/product/entity/product-b.spec.ts
@@ -1,33 +1,33 @@
-import Product from "./product";
+import ProductB from "./product-b";
 
-describe("Product unit tests", () => {
+describe("ProductB unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
-      const product = new Product("", "Product 1", 100);
+      const product = new ProductB("", "Product 1", 100);
     }).toThrowError("product: Id is required");
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
-      const product = new Product("123", "", 100);
+      const product = new ProductB("123", "", 100);
     }).toThrowError("product: Name is required");
   });
 
   it("should throw error when price is less than zero", () => {
     expect(() => {
-      const product = new Product("123", "Name", -1);
+      const product = new ProductB("123", "Name", -1);
     }).toThrowError("product: Price must be greater than zero");
   });
 
   it("should change name", () => {
-    const product = new Product("123", "Product 1", 100);
+    const product = new ProductB("123", "Product 1", 100);
     product.changeName("Product 2");
     expect(product.name).toBe("Product 2");
   });
 
   it("should change price", () => {
-    const product = new Product("123", "Product 1", 100);
+    const product = new ProductB("123", "Product 1", 100);
     product.changePrice(150);
-    expect(product.price).toBe(150);
+    expect(product.price).toBe(300);
   });
-});
+}); 
--- src/usecase/product/update/update.product.usecase.ts
+++ src/usecase/product/update/update.product.usecase.ts
@@ -1,5 +1,7 @@
 import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
 import { InputUpdateProductDto, OutputUpdateProductDto } from "./update.product.dto";
+import Product from "../../../domain/product/entity/product";
+import NotificationError from "../../../domain/@shared/notification/notification.error";
 
 export default class UpdateProductUseCase {
   private productRepository: ProductRepositoryInterface;
@@ -10,6 +12,12 @@ export default class UpdateProductUseCase {
 
   async execute(input: InputUpdateProductDto): Promise<OutputUpdateProductDto> {
     const product = await this.productRepository.find(input.id);
+    
+    // Validate input before updating
+    const tempProduct = new Product(input.id, input.name, input.price);
+    if (tempProduct.notification.hasErrors()) {
+      throw new NotificationError(tempProduct.notification.getErrors());
+    }
     
     product.changeName(input.name);
     product.changePrice(input.price);
